1bc7fa19a050968c144e73fbdf9dd81b216a09a4
siery <siery@comic.com>
2024-06-27 02:17:50 +0200

    Handle crystal files as ruby ts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Set .cr files to be detected as Ruby files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.cr",
+  command = "setfiletype ruby",
+})
