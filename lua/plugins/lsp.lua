8fb1cd196f8676120895259427d2fdc75cdd14ea
siery <siery@comic.com>
2024-12-19 05:44:51 +0100

    Update ccl

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,6 +13,13 @@ return {
           root_dir = require("lspconfig").util.find_git_ancestor,
           single_file_support = true,
         },
+        c = {
+          cmd = {
+            "ccls",
+          },
+          filetype = { "c" },
+          single_file_support = true,
+        },
       },
     },
   },
